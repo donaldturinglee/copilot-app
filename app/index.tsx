632fc2506f1fc03a8ac8d39cf4cfd2b55979d900
donaldturinglee <donaldturinglee@gmail.com>
2024-12-13 04:58:57 -0800

    feat: Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+
+export default function App() {
+  return (
+      <View>
+          <Text>
+              Copilot
+          </Text>
+      </View>
+  );
+}
